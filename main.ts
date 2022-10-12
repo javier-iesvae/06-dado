d875b4d328bc900c1b46288f00254e1d1ae4852c
javier-iesvae <114832717+javier-iesvae@users.noreply.github.com>
2022-10-12 12:31:14 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,30 @@
-let n = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(input.temperature())
+})
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     n = randint(1, 6)
     if (n == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+        basic.showString("1")
     } else if (n == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . . . #
-            . . . . .
-            . . . . .
-            `)
+        basic.showString("2")
     } else if (n == 3) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
+        basic.showString("3")
     } else if (n == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
+        basic.showString("4")
     } else if (n == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
+        basic.showString("5")
     } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+        basic.showString("6")
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
+let n = 0
+basic.showString("HOLA")
